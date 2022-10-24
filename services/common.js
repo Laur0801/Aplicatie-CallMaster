b7f9ff74b21476988bc8265e68a54e52766fb328
hayzamjs <hayzam@gmail.com>
2022-10-24 06:12:37 +0530

    Conf: write default gateway to extensions.conf

--- services/common.js
+++ services/common.js
@@ -26,11 +26,16 @@ async function getExtensions (count = false) {
   })
 }
 
-async function getTrunks (count = false) {
+async function getTrunks (count = false, isDefault = false) {
   return connect(async db => {
     if (count) {
       return await db.get('SELECT COUNT(*) as count FROM trunks')
     }
+
+    if (isDefault) {
+      return await db.get('SELECT * FROM trunks WHERE isDefault = 1')
+    }
+
     return await db.all('SELECT * FROM trunks')
   })
 }
@@ -59,9 +64,18 @@ async function commitChanges (startup = false) {
     }
   }
 
+  /* Check if there's a default gateway */
+  const defaultTrunk = await getTrunks(false, true);
+  let [includeTrunkChunk, dialoutChunk] = [``,``];
+
+  if (defaultTrunk !== []) {
+    includeTrunkChunk = `include => dialout\n\n`;
+    dialoutChunk = `[dialout]\nexten => _X.,1,DIAL(SIP/\${EXTEN}@${defaultTrunk.name})\nexten => _X.,n,Hangup()\n\nexten => _+X.,1,Dial(SIP/\${EXTEN}@${defaultTrunk.name})\nexten => _+X.,n,Hangup()\n\n`;
+  }
+  
   await Promise.all([
     fs.writeFile(asteriskConfig.sipConf, '[general]\nbindaddr=0.0.0.0\nbindport=5060\nallowguest=yes\nallow=all\nallow=ulaw\nallow=alaw\nallow=g722\nallow=g729\nallowguest=yes\nnat=no\ntcpenable=no'),
-    fs.writeFile(asteriskConfig.extensionsConf, '[sip_internal]\nexten => _X.,1,Dial(SIP/$' + '{EXTEN}' +')' + '\n'),
+    fs.writeFile(asteriskConfig.extensionsConf, `[sip_internal]\n${includeTrunkChunk}exten => _X.,1,Dial(SIP/$` + '{EXTEN}' + ')' + '\n'),
     fs.writeFile(asteriskConfig.queuesConf, '')
   ])
 
@@ -77,13 +91,10 @@ async function commitChanges (startup = false) {
     getQueues()
   ])
 
-  /* Writing zyvo-sip.conf */
-
   for (let i = 0; i < extensions.length; i++) {
     finalOutputSip += `\n\n[${extensions[i].extension}]\ncallerid="${extensions[i].name}" <${extensions[i].extension}>\nsecret=${extensions[i].secret}\nallow=all\ntype=peer\ntrusttrpid=yes\nsendrpid=yes\nrpid_update=yes\nhost=dynamic\ncanreinvite=no\ncontext=sip_internal\ndtmfmode=rfc2833\ndtml=rfc2833\n`
   }
 
-  /* Writing zyvo-sip.conf */
   for (let i = 0; i < trunks.length; i++) {
     const codecsArr = trunks[i].codecs.split(',')
     let codecsStr = ''
@@ -112,8 +123,6 @@ async function commitChanges (startup = false) {
     finalOutputSip += `\n\n[${trunks[i].name}]\ntype=${trunks[i].type}\ncontext=${trunks[i].context}\nhost=${trunks[i].host}\nqualify=${trunks[i].qualify}\ncanreinvite=${trunks[i].canreinvite}\ninsecure=${trunks[i].insecure}\ndisallow=all\n${codecsStr}\n${portSecretStr}`
   }
 
-  /* Writing zyvo-queues.conf */
-
   for (let i = 0; i < queues.length; i++) {
     const members = queues[i].members.split(',')
     let membersStr = ''
@@ -124,11 +133,9 @@ async function commitChanges (startup = false) {
     finalOutputQueues += `\n[${queues[i].name}]\nmusicclass=default\nstrategy=${queues[i].strategy}\ntimeout=${queues[i].timeout}\nwrapuptime=${queues[i].wrapuptime}\nautopause=${queues[i].autopause}\n${membersStr}`
   }
 
-  /* Writing zyvo-extensions.conf */
-
   finalOutputSip += '\n\n#include sip_zyvo_user.conf\n'
-  finalOutputExtensions += '\n\n#include extensions_zyvo_user.conf\n'
-  finalOutputQueues += '\n\n#include queues_zyvo_user.conf\n'
+  finalOutputExtensions += `\n${dialoutChunk}\n\n#include extensions_zyvo_user.conf\n`
+  finalOutputQueues += '#include queues_zyvo_user.conf\n'
 
   await Promise.all([
     fs.appendFile(asteriskConfig.sipConf, finalOutputSip),
